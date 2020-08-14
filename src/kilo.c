2fa9c8754acab32db75501e81b30d611b72b31ee
waves <986299679@qq.com>
2020-08-14 11:31:04 +0800

    Avoiding waiting for input indefinately

--- src/kilo.c
+++ src/kilo.c
@@ -22,11 +22,18 @@ int main(void)
   enableRawMode();
 
   char c;
-  while (1 == read(STDIN_FILENO, &c, 1) && c != 'q') {
+  while (1) {
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
+
     if (iscntrl(c)) {
       printf("%d\r\n", c);
     } else {
       printf("%d (%c)\r\n", c, c);
+    }
+
+    if ('q' == c) {
+      break;
     }
   }
 
@@ -39,11 +46,14 @@ void enableRawMode()
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-  raw.c_iflag &= ~(ICRNL | IXON | BRKINT | INPCK | ISTRIP);                  // Disable <C-s> and <C-q> so that can display it | Fix <C-m>
-  raw.c_oflag &= ~(OPOST);                         // Make a \r\n
+  raw.c_iflag &= ~(ICRNL | IXON | BRKINT | INPCK | ISTRIP); // Disable <C-s> and <C-q> so that can display it | Fix <C-m>
+  raw.c_oflag &= ~(OPOST); // Make a \r\n
   raw.c_cflag |= ~(CS8);
   raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN); // Disable echo | Enable runtime refect | Disable <C-c> |
                                                    // Disable <C-v>
+  raw.c_cc[VMIN] = 0;  // Set the mininal number of bytes of input needed for read(), 0 for return as soon when input.
+  raw.c_cc[VTIME] = 1; // Set the max time to wait for for read(), it is in tenth of a second, so 1 is 1/10 second.
+
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
