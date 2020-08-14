77e6d237614fa536b3f886b54aae3a2677c97efb
waves <986299679@qq.com>
2020-08-14 14:50:56 +0800

    fix the problem cannot quit

--- src/kilo.c
+++ src/kilo.c
@@ -64,7 +64,7 @@ char editorReadKey()
   int nread;
   char c;
 
-  while ((nread = read(STDIN_FILENO, &c, 1))) {
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
     if (nread == -1 && errno != EAGAIN) {
       die("read");
     }
