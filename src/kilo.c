f516558e14a7249a0fabd95785391a5b364f0cdf
waves <986299679@qq.com>
2020-08-15 19:46:43 +0800

    1. Make the cursor hidden when painting the `~`; 2. Fix the bug that not free the buffer.

--- src/kilo.c
+++ src/kilo.c
@@ -126,13 +126,17 @@ void editorRefreshScreen()
 {
   struct abuf ab = ABUF_INIT;
 
+  abAppend(&ab, "\x1b[?25l", 6); // Hide the cursor whne painting the `~`
   abAppend(&ab, "\x1b[2J", 4); // Clean the screen
   abAppend(&ab, "\x1b[H", 3);  // Put the cursor to first line first character.
 
   editorDrawRows(&ab);
 
   abAppend(&ab, "\x1b[H", 3);
+  abAppend(&ab, "\x1b[?25h", 6); // Show the cursor as long as painting finished
+
   write(STDOUT_FILENO, ab.b, ab.len);
+  abFree(&ab); // Do not forget to free
 }
 
 void editorDrawRows(struct abuf *buf)
