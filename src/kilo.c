c072b20070347a688ddd613e3e99bdd33695fe31
waves <986299679@qq.com>
2020-08-21 10:26:11 +0800

    Add dirty flag and show if the buffer changed in the status line

--- src/kilo.c
+++ src/kilo.c
@@ -62,6 +62,8 @@ struct editorConfig {
   int coloff;
   int screenrows;
   int screencols;
+  /*buffer modified  flag*/
+  int dirty;
   /*line viewer*/
   int numrows;
   Erow *row;
@@ -161,6 +163,7 @@ void initEditor()
   E.cx = 0;
   E.cy = 0;
   E.rx = 0;
+  E.dirty = 0;
   E.rowoff = 0;
   E.coloff = 0;
   E.numrows = 0;
@@ -390,8 +393,9 @@ void editorDrawRows(struct abuf *buf)
 void editorDrawStatusBar(struct abuf *ab)
 {
   char status[80], rstatus[80];
-  int len = snprintf(status, sizeof(status), "%.20s - %d lines",
-                     E.filename ? E.filename : "[NO NAME]", E.numrows);
+  int len = snprintf(status, sizeof(status), " | %.20s - %d lines %s",
+                     E.filename ? E.filename : "[NO NAME]", E.numrows,
+                     E.dirty ? " " : "");
   int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
 
   if (len > E.screencols) {
@@ -507,6 +511,7 @@ void editorAppendRow(char *s, size_t len)
   editorUpdateRow(&E.row[at]);
 
   E.numrows++;
+  E.dirty++;
 }
 
 int editorRowCxToRx(Erow *row, int cx)
@@ -535,6 +540,7 @@ void editorRowInsertChar(Erow *row, int at, int c)
   row->size++;
   row->chars[at] = c;
   editorUpdateRow(row);
+  E.dirty++;
 }
 
 void editorInsertChar(int c)
@@ -572,6 +578,7 @@ void editorOpen(char *filename)
 
   free(line);
   fclose(fp);
+  E.dirty = 0;
 }
 
 // pointer buflen just get the lenth of buf
@@ -614,6 +621,7 @@ void editorSave()
       if (write(fd, buf, len) == len) {
         close(fd);
         free(buf);
+        E.dirty = 0;
         editorSetStatusMessage("%d bytes written to disk.", len);
         return;
       }
